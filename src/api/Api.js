57f12a005bacf3427260052f9c4ecf4908dc778b
Ameesha <ameesha.gireeshkm@gmail.com>
2021-08-01 17:41:17 +0400

    feat: add live preview

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const API = axios.create({
-    baseURL: "http://192.168.1.2:3001",//PROCESS.ENV_REACT_APP_BASE_URL
+    baseURL: "http://localhost:3001", //PROCESS.ENV_REACT_APP_BASE_URL
     responseType: "json",
 })
 
--- src/pages/Eventform/index.jsx
+++ src/pages/Eventform/index.jsx
@@ -3,23 +3,32 @@ import "./styles.css";
 import { BASEURL } from "../../api/Api"
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Link } from "react-router-dom";
+import EventProfile from "../../components/EventProfile";
 
 const EventForm = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [img, setImg] = useState("https://external-preview.redd.it/fBai3DKZrSGf3YRd89f9pUHJua_lyGNS3LF0I-joA8Y.jpg?width=640&crop=smart&auto=webp&s=d53d81a152b17cbb467151b7513a7e0aac378567");
+  const [org, setOrg] = useState("Club/Chapter");
+  const [title, setTitle] = useState("Event Title");
+  const [email, setEmail] = useState("example@example.com");
+  const [desc, setDesc] = useState("Description of the Event");
+  const [url, setURL] = useState("URL of the Event");
+
   return (
     <div className="flex md:container md:mx-auto items-center">
       <div className="container" >
         <form action={`${BASEURL}/api/add`} method="POST">
           <div className="row">
             <h4>Event Details</h4>
-            <br/>
+            <br />
             <div className="input-group" >
               <input
                 type="text"
                 placeholder="Event Name"
                 name="title"
-                required="required"
+                required="required" onChange={(e) => setTitle(e.target.value)}
               />
               <div className="input-icon">
                 <i className="fa fa-user"></i>
@@ -31,7 +40,7 @@ const EventForm = () => {
                 placeholder="Email"
                 name="email"
                 pattern="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
-                required
+                onChange={(e) => setEmail(e.target.value)} required
               />
               <div className="input-icon">
                 <i className="fa fa-user"></i>
@@ -42,7 +51,7 @@ const EventForm = () => {
                 type="text"
                 placeholder="Club/Chapter Name"
                 name="org"
-                required
+                required onChange={(e) => setOrg(e.target.value)}
               />
               <div className="input-icon">
                 <i className="fa fa-envelope"></i>
@@ -52,7 +61,7 @@ const EventForm = () => {
               <textarea
                 placeholder="Event Description"
                 name="desc" className="description w-full px-4 pt-4 resize:none"
-                required
+                required onChange={(e) => setDesc(e.target.value)}
               />
               <div className="input-icon">
                 <i className="fa fa-user"></i>
@@ -62,33 +71,27 @@ const EventForm = () => {
           </div>
           <div className="row">
           </div>
-          <div className="col-half">
+          <div className="col-half h-16 flex flex-wrap content-start">
             <h4>Label Color </h4>
-            <div className="event-color">
-              <input
-                type="color"
-                placeholder="Label Color"
-                name="backgroundColor" height="50"
-                required style={{ height: "60px" }}
-              />
-            </div>
+            <input className="label-color"
+              type="color"
+              placeholder="Label Color"
+              name="backgroundColor"
+              required style={{ height: "60px" }}
+            />
           </div>
-          <div className="col-half">
+
+          <div className="col-half h-16 flex flex-wrap content-start">
+
             <h4>Text Color </h4>
-            <div className="text-color">
-              <input
-                type="color"
-                placeholder="Text Color"
-                name="textColor"
-                required style={{ height: "60px" }}
-              />
-            </div>
+            <input className="text-color"
+              type="color"
+              placeholder="Text Color"
+              name="textColor"
+              required style={{ height: "60px" }}
+            />
           </div>
 
-          <div className="input-group"></div>
-
-          <div className="row">
-          </div>
           <div className="col-full">
             <h4>Start Date and Time of Event (IST +5:30) </h4>
             <div className="input-group">
@@ -125,13 +128,13 @@ const EventForm = () => {
             <div className="input-group"></div>
           </div>
           <div className="input-group">
-            <input type="text" placeholder="Image URL" name="img" />
+            <input type="text" placeholder="Image URL" name="img" onChange={(e) => setImg(e.target.value)}/>
             <div className="input-icon">
               <i className="fa fa-user"></i>
             </div>
           </div>
           <div className="input-group">
-            <input type="text" placeholder="Event URL" name="url" />
+            <input type="text" placeholder="Event URL" name="url" onChange={(e) => setURL(e.target.value)}/>
             <div className="input-icon">
               <i className="fa fa-user"></i>
             </div>
@@ -143,7 +146,9 @@ const EventForm = () => {
           </div>
         </form>
       </div>
-    </div>
+       <EventProfile  img={img} org={org} title={title} start={startDate} end={endDate} email={email} desc={desc} url={url}/>;
+
+  </div>
   );
 };
 
